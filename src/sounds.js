77ca84827338175fdff7866d96870ea54eb748b3
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-07-02 19:09:06 -0400

    Add functions for hh's and aux's

--- src/sounds.js
+++ src/sounds.js
@@ -1,3 +1,15 @@
+// create an array frequencies correspoindgin to MIDI notes
+let A = 440.0;
+let midiNotes = [];
+for (let i = 0; i < 127; i++) {
+  midiNotes[i] = A * Math.pow(2, (i - 69) / 12);
+}
+
+// convert MIDI note to frequency
+const midiToFreq = note => {
+  return A * Math.pow(2, (note - 69) / 12);
+};
+
 // Create and return a buffer filled with noise (aka random values)
 const noiseBuffer = context => {
   let bufferSize = context.sampleRate;
@@ -16,7 +28,7 @@ function kick(context, buffer, time) {
   const gain = context.createGain();
 
   osc.type = 'triangle';
-  osc.frequency.value = 30;
+  osc.frequency.value = 70;
   osc.connect(gain);
 
   gain.connect(context.destination);
@@ -101,18 +113,110 @@ function tom2(context, time) {
 }
 
 function hhopen(context, time) {
+  const noise = context.createBufferSource();
+  noise.buffer = noiseBuffer(context);
+  const noiseGain = context.createGain();
+  noiseGain.gain.value = 0.5;
+
+  const osc = context.createOscillator();
+  osc.frequency.value = 2000;
+  const oscGain = context.createGain();
+  oscGain.gain.value = 0.2;
+
+  const gain = context.createGain();
+
+  noise.connect(noiseGain);
+  noiseGain.connect(gain);
+
+  osc.connect(oscGain);
+  oscGain.connect(gain);
+
+  gain.connect(context.destination);
+  gain.gain.value = 0;
+
+  noise.start(context.currentTime);
+  osc.start(context.currentTime);
+
+  const LENGTH = 0.5;
+  gain.gain.linearRampToValueAtTime(0.7, context.currentTime + 0.001);
+  gain.gain.setValueCurveAtTime([0.7, 0], context.currentTime + 0.001, LENGTH);
   console.log('HH Open!');
 }
 
 function hhclosed(context, time) {
+  const noise = context.createBufferSource();
+  noise.buffer = noiseBuffer(context);
+  const noiseGain = context.createGain();
+  noiseGain.gain.value = 0.5;
+
+  const osc = context.createOscillator();
+  osc.type = 'triangle';
+  osc.frequency.value = 2000;
+  const oscGain = context.createGain();
+  oscGain.gain.value = 0.2;
+
+  const gain = context.createGain();
+
+  noise.connect(noiseGain);
+  noiseGain.connect(gain);
+
+  osc.connect(oscGain);
+  oscGain.connect(gain);
+
+  gain.connect(context.destination);
+  gain.gain.value = 0;
+
+  noise.start(context.currentTime);
+  osc.start(context.currentTime);
+
+  const LENGTH = 0.1;
+  gain.gain.linearRampToValueAtTime(0.7, context.currentTime + 0.001);
+  gain.gain.setValueCurveAtTime([0.7, 0], context.currentTime + 0.001, LENGTH);
   console.log('HH Closed!');
 }
 
 function aux1(context, time) {
+  const osc = context.createOscillator();
+  osc.type = 'triangle';
+  const gain = context.createGain();
+
+  // osc.type = '';
+  let randomNote = (Math.random() * 30 + 30) | 0;
+  osc.frequency.value = midiToFreq(randomNote);
+  osc.frequency.linearRampToValueAtTime(midiToFreq(randomNote), context.currentTime + 0.1);
+  gain.connect(context.destination);
+  gain.gain.value = 0;
+  osc.start(context.getCurrentTime);
+  osc.connect(gain);
+
+  const LENGTH = 2;
+  gain.gain.linearRampToValueAtTime(0.9, context.currentTime + 0.2);
+  gain.gain.setValueCurveAtTime([0.9, 0], context.currentTime + 0.2, LENGTH);
+  osc.stop(context.currentTime + LENGTH + 0.4);
+
   console.log('Aux 1!');
 }
 
 function aux2(context, time) {
+  const osc = context.createOscillator();
+  osc.type = 'triangle';
+  const gain = context.createGain();
+
+  // osc.type = '';
+  let randomNote = (Math.random() * 30 + 50) | 0;
+  osc.frequency.value = midiToFreq(randomNote);
+  osc.frequency.linearRampToValueAtTime(midiToFreq(randomNote), context.currentTime + 0.1);
+  gain.connect(context.destination);
+  gain.gain.value = 0;
+  osc.start(context.getCurrentTime);
+  osc.connect(gain);
+
+  const LENGTH = 2;
+  gain.gain.linearRampToValueAtTime(0.9, context.currentTime + 0.2);
+  gain.gain.setValueCurveAtTime([0.9, 0], context.currentTime + 0.2, LENGTH);
+  osc.stop(context.currentTime + LENGTH + 0.4);
+
+  console.log('Aux 1!');
   console.log('Aux 2!');
 }
 
